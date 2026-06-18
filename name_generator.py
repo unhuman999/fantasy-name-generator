ed01b61eef85e7594d11dc6a42921e10c7a62a01
Unhuman999 <rpoludnenko@gmail.com>
2026-06-18 13:24:53 +0300

    change list to dictionary for races data

--- name_generator.py
+++ name_generator.py
@@ -1,20 +1,34 @@
-orc_male = ["Azgoth", "Borgakh", "Durgash", "Grommash", "Khazuk", "Mogdurz", "Nagrul", "Ruggoth", "Thokrak", "Uzgash"]
-orc_female = ["Agtha", "Borga", "Duhgasha", "Gorshka", "Khagra", "Lagak", "Mugra", "Nagra", "Sharga", "Urzga"]
-elf_male = ["Aelar", "Thalanil", "Faelivrin", "Kaelen", "Ornthalas", "Rielan", "Sildor", "Theron", "Valandris", "Zylarien"]
-elf_female = ["Aelindel", "Celethiel", "Elowen", "Faerûna", "Laeral", "Merilwen", "Nimphiel", "Sariel", "Thaviel", "Yavanna"]
-human_male = ["Aldric", "Brennan", "Corvin", "Darian", "Evander", "Gregor", "Haldric", "Loric", "Randver", "Theron"]
-human_female = ["Adara", "Brigid", "Caelia", "Elara", "Gwendolyn", "Isolde", "Linette", "Rosamund", "Seren", "Theodora"]
+
+races = {
+    "orc":{
+        "male":["Azgoth", "Borgakh", "Durgash", "Grommash", "Khazuk", "Mogdurz", "Nagrul", "Ruggoth", "Thokrak", "Uzgash"],
+        "female":["Agtha", "Borga", "Duhgasha", "Gorshka", "Khagra", "Lagak", "Mugra", "Nagra", "Sharga", "Urzga"]
+    },
+    "elf":{
+        "male":["Aelar", "Thalanil", "Faelivrin", "Kaelen", "Ornthalas", "Rielan", "Sildor", "Theron", "Valandris", "Zylarien"],
+        "female":["Aelindel", "Celethiel", "Elowen", "Faeruna", "Laeral", "Merilwen", "Nimphiel", "Sariel", "Thaviel", "Yavanna"]
+    },
+    "human":{
+        "male":["Aldric", "Brennan", "Corvin", "Darian", "Evander", "Gregor", "Haldric", "Loric", "Randver", "Theron"],
+        "female":["Adara", "Brigid", "Caelia", "Elara", "Gwendolyn", "Isolde", "Linette", "Rosamund", "Seren", "Theodora"]
+    }
+}
+
+
+
 def show_names(names):
     for name in names:
         print(name)
-def count_name(names):
+def count_names(names):
     print(f"Total: {len(names)}")
 def race_info(names):
     show_names(names)
-    count_name(names)
-race_info(orc_male)
-race_info(orc_female)
-race_info(elf_male)
-race_info(elf_female)
-race_info(human_male)
-race_info(human_female)
+    count_names(names)
+
+
+race_info(races["orc"]["male"])
+race_info(races["orc"]["female"])
+race_info(races["elf"]["male"])
+race_info(races["elf"]["female"])
+race_info(races["human"]["male"])
+race_info(races["human"]["female"])
